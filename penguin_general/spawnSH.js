6931da02c6d05795f7dfb90a2d57e712bd5efc6b
akowalski97 <akowalski@flextech-hs.org>
2015-04-28 07:45:22 -0700

    asdf

--- penguin_general/spawnSH.js
+++ penguin_general/spawnSH.js
@@ -1,10 +1,10 @@
 exports.spawnSH = function (player) {
-    box('4', 9, 1, 9)
+    box('cobblestone', 9, 1, 9)
         .move('start')
-        .box0('4', 9, 6, 9)
+        .box0('cobblestone', 9, 6, 9)
         .move('start')
         .up(6)
-        .prism('5', 9, 9)
+        .prism('oak', 9, 9)
         .move('start')
         .right(4)
         .up(1)
